da472066a25ff3eb1dc9f56743fac1e2130d98c7
unknown <gedison.ge@gmail.com>
2016-10-19 18:07:14 -0400

    Tile scripts/basic environment

--- Assets/script/tile/Tile.cs
+++ Assets/script/tile/Tile.cs
@@ -5,13 +5,10 @@ public abstract class Tile : MonoBehaviour {
     public Texture tileTexture;
 
     void Start() {
-
-      
         if (GetComponent<Renderer>().material.name.Contains("Default")) {
             if (tileTexture != null) GetComponent<Renderer>().material.mainTexture = tileTexture;
             else GetComponent<Renderer>().material.mainTexture = Resources.Load("default", typeof(Texture2D)) as Texture2D;
         }
-       
     }
 
     public abstract int getMovementModifier();
