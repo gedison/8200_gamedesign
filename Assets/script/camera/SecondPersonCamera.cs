234be811d7f666baff56201bb4e94a017cfa355a
gedison <gedison.ge@gmail.com>
2016-10-21 11:25:25 -0400

    working update

---
+++ Assets/script/camera/SecondPersonCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondPersonCamera : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset = new Vector3(2.5f, 3.8f, 0f); 
+    private float cameraSpeed;
+
+    void Start() {
+        cameraSpeed = player.GetComponent<CharacterMovementController>().movement - 1;
+        offset.y += player.GetComponent<CharacterMovementController>().playerHeight;
+    }
+
+    void LateUpdate() {
+        //transform.rotation = Quaternion.identity;
+        transform.position = Vector3.Lerp(this.transform.position, player.transform.position + offset, cameraSpeed * Time.deltaTime);
+    }
+}
