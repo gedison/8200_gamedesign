a7aab7bf20bd642b192ede0be11fb1eb513f19b7
Galen Helfter <ghelfter@gmail.com>
2016-11-01 21:42:30 -0400

    Added experience manager

---
+++ Assets/script/profession/ExperienceManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExperienceManager : MonoBehaviour {
+	int[] experiences = {200, 500, 1000, 2000};
+
+	// Determine what level the character might be
+	public bool hasLeveledUp(int level, int experience) {
+		bool result = false;
+		for(int i = 0; i < experience.Length; ++i) {
+			if (level == i && experience > experiences [i]) {
+				result = true;
+			}
+		}
+
+		return result;
+	}
+
+	public void setExperience(int[] nExperiences) {
+		experiences = nExperiences;
+	}
+
+	public void alterExperienceAt(int value, int loc) {
+		experiences [loc] = value;
+	}
+}
