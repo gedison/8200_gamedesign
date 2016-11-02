73ca0f5d421673a13f23320a81c9a6a214111372
gedison <gedison.ge@gmail.com>
2016-11-01 23:24:37 -0400

    Basic AI Update

--- Assets/script/character/EnemyPrototype.cs
+++ Assets/script/character/EnemyPrototype.cs
@@ -2,8 +2,6 @@
 using System.Collections;
 
 public class EnemyPrototype : CharacterController {
-	private int[] tilesEffectedByPlayerSkill;
-
 
     new void Start() {
         base.Start();
@@ -13,45 +11,27 @@ public class EnemyPrototype : CharacterController {
 	void Update () {
 
 		WorldController ctrl = WorldController.instance;
+        int playerTileInstanceID = ctrl.player.GetComponent<CharacterPosition>().getCurrentInstanceID();
+        int playerTile = ctrl.getTileIndexFromID(playerTileInstanceID);
 
-
-        int playerTileInstanceID = WorldController.instance.player.GetComponent<CharacterPosition>().getCurrentInstanceID();
-        int playerTile = WorldController.instance.getTileIndexFromID(playerTileInstanceID);
         if (this.getCurrentCharacterState() == CharacterState.MOVE) {
             if (isTileWithinRangeOfCurrentSkill(playerTile)) {
                 Debug.Log("NPC IN RANGE: SWITCH TO ATTACK");
                 setCurrentCharacterState(CharacterState.ATTACK);
             } else {
                 Debug.Log("NPC OUT OF RANGE MOVE");
-                WorldController.instance.onTileHover(playerTileInstanceID);
-                WorldController.instance.onTileSelect(playerTileInstanceID);
+                ctrl.onTileHover(playerTileInstanceID);
+                ctrl.onTileSelect(playerTileInstanceID);
             }
+
             if (getCurrentActionPoints() < 2) setActionPointsToZero();
         }else if(this.getCurrentCharacterState() == CharacterState.ATTACK) {
             Debug.Log("NPC ATTACK");
-            WorldController.instance.onTileHover(playerTileInstanceID);
-            WorldController.instance.onTileSelect(playerTileInstanceID);
+            ctrl.onTileHover(playerTileInstanceID);
+            ctrl.onTileSelect(playerTileInstanceID);
+
             if (getCurrentActionPoints() < 3) setActionPointsToZero();
         }
-            /*
-			int ploc = WorldController.instance.getTileIndexFromID (WorldController.instance.player.GetComponent<CharacterPosition> ().getCurrentInstanceID ());
-			int enemyPosition = ctrl.getTileIndexFromID(ctrl.player.GetComponent<CharacterPosition>().getCurrentInstanceID());
-			tilesEffectedByPlayerSkill = getTilesEffectedByCurrentSkill(ploc);
-			for (int i = 0; i < tilesEffectedByPlayerSkill.Length; i++) {
-				if (tilesEffectedByPlayerSkill[i] == enemyPosition) {
-					setCurrentCharacterState (CharacterState.ATTACK);
-				}
-			}
-            */
-
-			//WorldController.instance.onTileHover (WorldController.instance.player.GetComponent<CharacterPosition> ().getCurrentInstanceID ());
-			//WorldController.instance.onTileSelect (WorldController.instance.player.GetComponent<CharacterPosition> ().getCurrentInstanceID ());
-
-			//if (getCurrentActionPoints() < 3) setCurrentCharacterState(CharacterState.ATTACK);
-		}
-
-       // this.setActionPointsToZero();
-
-
-    }
+	}
+}
 
