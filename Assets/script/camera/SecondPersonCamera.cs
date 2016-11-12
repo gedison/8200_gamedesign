2972bef3aaa200a8215969cb7290d804da530044
gedison <gedison.ge@gmail.com>
2016-11-12 12:37:10 -0500

    Working Update UI

--- Assets/script/camera/SecondPersonCamera.cs
+++ Assets/script/camera/SecondPersonCamera.cs
@@ -4,16 +4,39 @@ using System.Collections;
 public class SecondPersonCamera : MonoBehaviour {
 
     public GameObject player;
+    public float mouseSensitivity = .1f;
+    private Vector3 lastPosition;
     private Vector3 offset = new Vector3(2.5f, 3.8f, 0f); 
     private float cameraSpeed;
+    private bool isMoving = false;
+
 
     void Start() {
         cameraSpeed = player.GetComponent<CharacterMovementController>().movementSpeed - 1;
         offset.y += player.transform.position.y;
     }
 
+    void Update() {
+        if (Input.GetMouseButtonDown(1)) {
+            lastPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(1)) {
+            Vector3 delta = Input.mousePosition - lastPosition;
+            transform.Translate(delta.y * mouseSensitivity, 0, -delta.x * mouseSensitivity, Space.World);
+            lastPosition = Input.mousePosition;
+        }
+    }
+
     void LateUpdate() {
-        if(player!=null)
+        if(player!=null && player.GetComponent<CharacterMovementController>().isCharacterMoving())isMoving = true;
+
+
+        if (player != null &&  isMoving) {
             transform.position = Vector3.Lerp(this.transform.position, player.transform.position + offset, cameraSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, (player.transform.position+offset)) < .5) isMoving = false;
+        }
+        
+            
     }
 }
---
+++ Assets/script/ui/CharacterUIController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CharacterUIController : MonoBehaviour {
+
+    public GameObject player;
+    public Slider healthBar;
+    public UICircle2 apBar;
+
+    private CharacterController myCharacterController;
+    private Health myCharacterHealth;
+
+    // Use this for initialization
+    void Start () {
+        myCharacterController = player.GetComponent<CharacterController>();
+
+    }
+
+    void Update() {
+        if (myCharacterHealth == null && player != null) {
+            myCharacterHealth = player.GetComponent<Health>();
+        }
+
+        if (player != null) {
+            if (myCharacterHealth != null) healthBar.value = ((float)myCharacterHealth.getCurrentHealth() / myCharacterHealth.totalHealth);
+            float test = ((float)myCharacterController.getCurrentActionPoints() / myCharacterController.totalActionPoints);
+            int apPercent = (int)(test * 100);
+            apBar.newPercent = (apPercent);
+        }
+    }
+}
