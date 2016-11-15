258ac9a8f434e7bc90b8594ae2828132d71febd2
gedison <gedison.ge@gmail.com>
2016-11-15 17:05:40 -0500

    Working Update Camera

--- Assets/script/camera/SecondPersonCamera.cs
+++ Assets/script/camera/SecondPersonCamera.cs
@@ -10,6 +10,8 @@ public class SecondPersonCamera : MonoBehaviour {
     private float cameraSpeed;
     private bool isMoving = false;
 
+    private float rotX = 60, rotY = -90, rotZ = 0;
+
 
     void Start() {
         cameraSpeed = player.GetComponent<CharacterMovementController>().movementSpeed - 1;
@@ -18,22 +20,36 @@ public class SecondPersonCamera : MonoBehaviour {
     }
 
     void Update() {
-        if (Input.GetMouseButtonDown(1)) {
+        if (Input.GetMouseButtonDown(2) || Input.GetMouseButtonDown(1)) {
             lastPosition = Input.mousePosition;
         }
 
-        if (Input.GetMouseButton(1)) {
+        /*
+        if (Input.GetMouseButton(2)) {
             Vector3 delta = Input.mousePosition - lastPosition;
+            Debug.Log("Delta X: " + delta.x * mouseSensitivity + " " + delta.y * mouseSensitivity + " " + delta.z * mouseSensitivity);
             transform.Translate(delta.y * mouseSensitivity, 0, -delta.x * mouseSensitivity, Space.World);
             lastPosition = Input.mousePosition;
         }
+        */
+        
+        if (Input.GetMouseButton(1)) {
+            Vector3 delta = Input.mousePosition - lastPosition;
+            Debug.Log("Delta X: " + delta.x * mouseSensitivity + " " + delta.y * mouseSensitivity + " " + delta.z * mouseSensitivity);
+            if (Vector3.Distance(transform.position, (player.transform.position + offset)) < 6) transform.RotateAround(player.transform.position, new Vector3(0, 1), -delta.x * .5f);
+
+            offset += (transform.position - (player.transform.position+offset));
+            lastPosition = Input.mousePosition;
+        }
+
+
     }
 
     void LateUpdate() {
         if(player!=null && player.GetComponent<CharacterMovementController>().isCharacterMoving())isMoving = true;
 
-
         if (player != null &&  isMoving) {
+            //transform.rotation = Quaternion.Euler(rotX, rotY, rotZ);
             transform.position = Vector3.Lerp(this.transform.position, player.transform.position + offset, cameraSpeed * Time.deltaTime);
             if (Vector3.Distance(transform.position, (player.transform.position+offset)) < .5) isMoving = false;
         }
