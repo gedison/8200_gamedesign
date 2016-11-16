c4c2a059d2fae47ddbaa0dcacb6aab6ee22f92b3
gedison <gedison.ge@gmail.com>
2016-11-15 20:08:58 -0500

    Updated Camera Controls

--- Assets/script/camera/SecondPersonCamera.cs
+++ Assets/script/camera/SecondPersonCamera.cs
@@ -9,8 +9,10 @@ public class SecondPersonCamera : MonoBehaviour {
     private Vector3 offset = new Vector3(2.5f, 3.8f, 0f); 
     private float cameraSpeed;
     private bool isMoving = false;
-
+    private bool panned = false;
     private float rotX = 60, rotY = -90, rotZ = 0;
+
+    private float yOffset = 0f;
 
 
     void Start() {
@@ -24,31 +26,35 @@ public class SecondPersonCamera : MonoBehaviour {
             lastPosition = Input.mousePosition;
         }
 
-        /*
-        if (Input.GetMouseButton(2)) {
-            Vector3 delta = Input.mousePosition - lastPosition;
-            Debug.Log("Delta X: " + delta.x * mouseSensitivity + " " + delta.y * mouseSensitivity + " " + delta.z * mouseSensitivity);
-            transform.Translate(delta.y * mouseSensitivity, 0, -delta.x * mouseSensitivity, Space.World);
-            lastPosition = Input.mousePosition;
-        }
-        */
         
         if (Input.GetMouseButton(1)) {
             Vector3 delta = Input.mousePosition - lastPosition;
-            Debug.Log("Delta X: " + delta.x * mouseSensitivity + " " + delta.y * mouseSensitivity + " " + delta.z * mouseSensitivity);
-            if (Vector3.Distance(transform.position, (player.transform.position + offset)) < 6) transform.RotateAround(player.transform.position, new Vector3(0, 1), -delta.x * .5f);
+            delta = delta * mouseSensitivity;
+            delta = Quaternion.Euler(0, 0, -yOffset) * delta;
+            transform.Translate(delta.y, 0, -delta.x, Space.World);
+            lastPosition = Input.mousePosition;
+
+            panned = true;
+        }
+        
+        
+        if (Input.GetMouseButton(2) && !panned) {
+            Vector3 delta = Input.mousePosition - lastPosition;
+            if (Vector3.Distance(transform.position, (player.transform.position + offset)) < 6) {
+                transform.RotateAround(player.transform.position, new Vector3(0, 1), delta.x * .5f);
+                yOffset += delta.x * .5f;
+            }
 
             offset += (transform.position - (player.transform.position+offset));
             lastPosition = Input.mousePosition;
         }
-
-
     }
 
     void LateUpdate() {
         if(player!=null && player.GetComponent<CharacterMovementController>().isCharacterMoving())isMoving = true;
 
         if (player != null &&  isMoving) {
+            panned = false;
             //transform.rotation = Quaternion.Euler(rotX, rotY, rotZ);
             transform.position = Vector3.Lerp(this.transform.position, player.transform.position + offset, cameraSpeed * Time.deltaTime);
             if (Vector3.Distance(transform.position, (player.transform.position+offset)) < .5) isMoving = false;
