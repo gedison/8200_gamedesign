e5a3c29e2ab1314b5df4ebcb0b18babe8e16e2ad
gedison <gedison.ge@gmail.com>
2016-11-27 11:10:39 -0500

    Working Update - Skill Tool Tips

---
+++ Assets/script/ui/tool tip/ToolTipTrigger.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+
+//http://www.sharkbombs.com/2015/02/10/tooltips-with-the-new-unity-ui-ugui/
+
+
+public class ToolTipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    
+
+    public GameObject toolTipPrefab;
+    public GameObject toolTip;
+    public Canvas canvas;
+
+    private bool shouldHoverBeStarted = true;
+
+    public void Start() {
+        toolTip = Instantiate(toolTipPrefab) as GameObject;
+        toolTip.transform.SetParent(canvas.transform, false);
+        toolTip.transform.position = new Vector3(transform.position.x, transform.position.y+110, transform.position.z);
+        toolTip.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        Invoke("StartHover", .75f);
+        shouldHoverBeStarted = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        shouldHoverBeStarted = false;
+        toolTip.SetActive(false);
+    }
+
+    private void StartHover() {
+        if (shouldHoverBeStarted) {
+            toolTip.SetActive(true);
+        }
+    }
+}
---
+++ Assets/script/ui/tool tip/ToolTipView.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTipView : MonoBehaviour {
+
+    public Text tooltipText;
+
+    private static ToolTipView instance;
+    public static ToolTipView Instance {
+        get {
+            if (instance == null)
+                instance = GameObject.FindObjectOfType<ToolTipView>();
+            return instance;
+        }
+    }
+
+    void Awake() {
+        instance = this;
+        HideToolTip();
+    }
+
+    public bool IsActive {
+        get {
+            return gameObject.activeSelf;
+        }
+    }
+
+    public void ShowToolTip(string text, Vector3 pos) {
+        if (tooltipText.text != text)
+            tooltipText.text = text;
+
+        transform.position = pos;
+
+        gameObject.SetActive(true);
+    }
+
+    public void HideToolTip() {
+        gameObject.SetActive(false);
+    }
+}
